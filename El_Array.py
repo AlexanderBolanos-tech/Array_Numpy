e44dd905dcd607f0f9cee8ebb918dbf924adc57e
Alexander Bolaños <alexander.bolanox@gmail.com>
2024-07-30 19:41:26 -0500

    se realizó repositorio en colad

---
+++ El_Array.py
@@ -0,0 +1,11 @@
+import numpy as np
+lista = [1,2,3,4,5,6,7,8,9]
+lista
+arr = np.array(lista)
+type(arr)
+matriz = [[1,2,3],[4,5,6],[7,8,9]]
+matriz = np.array(matriz)
+matriz
+arr
+arr [1]
+arr[0] + arr [5]
